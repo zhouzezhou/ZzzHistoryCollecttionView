e7959d5375d776a812f3b520d3dfe184bdcea82a
zhouzezhou <zhouze4zhou33@sina.com>
2018-05-22 22:36:11 +0800

    base code for UICollectionView

--- ZzzHistoryCollecttionView/ZzzHistoryCollecttionView/ViewController.m
+++ ZzzHistoryCollecttionView/ZzzHistoryCollecttionView/ViewController.m
@@ -8,7 +8,33 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+// 屏幕的宽度
+#define kScreenWidth [[UIScreen mainScreen] bounds].size.width
+
+// 屏幕的高度
+#define kScreenHeight [[UIScreen mainScreen] bounds].size.height
+
+// 系统状态栏高度
+#define kStatusBarHeight [UIApplication sharedApplication].statusBarFrame.size.height
+
+//设置RGB颜色值
+#define COLOR(R,G,B,A)    [UIColor colorWithRed:(CGFloat)R/255.0 green:(CGFloat)G/255.0 blue:(CGFloat)B/255.0 alpha:A]
+
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+
+//@property (nonatomic, strong) UICollectionView *mainCollectionView;
+
+// 计算cell size
+@property (nonatomic, assign) int rowCellNum;
+@property (nonatomic, assign) float cellPaddingAll;
+@property (nonatomic, assign) float cellWidth;
+@property (nonatomic, assign) float cellHeight;
+@property (nonatomic, assign) CGSize cellSize;
+
+// 计算一页的个数
+@property (nonatomic, assign) int onePageCellNum;
+
+
 
 @end
 
@@ -16,7 +42,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self.view setBackgroundColor:[UIColor redColor]];
+    
+    // 计算cell size
+    self.rowCellNum = 7;
+    self.cellPaddingAll = kScreenWidth / 10;
+    self.cellWidth = (kScreenWidth - self.cellPaddingAll) / self.rowCellNum;
+    self.cellHeight = self.cellWidth;
+    self.cellSize = CGSizeMake(self.cellWidth, self.cellHeight);
+    
+    // 计算一页的个数
+    self.onePageCellNum = (int)((kScreenHeight - kStatusBarHeight) / self.cellHeight) * self.rowCellNum;
+    
+    
+    UICollectionView *mainCollectionView;
+    
+    //1.初始化layout
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    //设置collectionView滚动方向
+    //    [layout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    //设置headerView的尺寸大小
+//    layout.headerReferenceSize = CGSizeMake(self.view.frame.size.width, 100);
+//    //该方法也可以设置itemSize
+//    layout.itemSize =CGSizeMake(110, 150);
+    
+    //2.初始化collectionView
+    mainCollectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    [self.view addSubview:mainCollectionView];
+    mainCollectionView.backgroundColor = [UIColor greenColor];
+    
+    //3.注册collectionViewCell
+    //注意，此处的ReuseIdentifier 必须和 cellForItemAtIndexPath 方法中 一致 均为 cellId
+    [mainCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellId"];
+    
+    //注册headerView  此处的ReuseIdentifier 必须和 cellForItemAtIndexPath 方法中 一致  均为reusableView
+//    [mainCollectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"reusableView"];
+    
+    //4.设置代理
+    mainCollectionView.delegate = self;
+    mainCollectionView.dataSource = self;
+    
+    
 }
 
 
@@ -25,5 +93,95 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark collectionView代理方法
+// 返回section个数
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+    return 1;
+}
+
+// 每个section的item个数
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.onePageCellNum;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UICollectionViewCell *cell = (UICollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:@"cellId" forIndexPath:indexPath];
+    
+//    cell.botlabel.text = [NSString stringWithFormat:@"{%ld,%ld}",(long)indexPath.section,(long)indexPath.row];
+    
+    cell.backgroundColor = [UIColor yellowColor];
+    
+    return cell;
+}
+
+//设置每个item的尺寸
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return self.cellSize;
+}
+
+//footer的size
+//- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section
+//{
+//    return CGSizeMake(10, 10);
+//}
+
+//header的size
+//- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
+//{
+//    return CGSizeMake(10, 10);
+//}
+
+//设置每个item的UIEdgeInsets
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+//    return UIEdgeInsetsMake(10, 10, 10, 10);
+    return UIEdgeInsetsZero;
+}
+
+//设置每个item水平间距
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+//    return 10;
+    return self.cellPaddingAll / (self.rowCellNum + 1);
+}
+
+
+//设置每个item垂直间距
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+//    return 15;
+    
+    return self.cellPaddingAll / (self.rowCellNum + 1);
+}
+
+
+//通过设置SupplementaryViewOfKind 来设置头部或者底部的view，其中 ReuseIdentifier 的值必须和 注册是填写的一致，本例都为 “reusableView”
+//- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+//{
+//    UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"reusableView" forIndexPath:indexPath];
+//    headerView.backgroundColor =[UIColor grayColor];
+//    UILabel *label = [[UILabel alloc] initWithFrame:headerView.bounds];
+//    label.text = @"这是collectionView的头部";
+//    label.font = [UIFont systemFontOfSize:20];
+//    [headerView addSubview:label];
+//    return headerView;
+//}
+
+//点击item方法
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
+{
+//    UICollectionViewCell *cell = (UICollectionViewCell *)[collectionView cellForItemAtIndexPath:indexPath];
+//    NSString *msg = cell.botlabel.text;
+//    NSLog(@"%@",msg);
+}
+
+
+
 
 @end
+
